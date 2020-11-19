ec3fe973c1e7704d26e95d1434cb0f70fab600f3
Andrew Snyder <ASnyder@minitab.com>
2020-11-19 12:29:12 -0500

    use pods, add proper timeout

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import signal
 import logging
+import threading
+
+from contextlib import contextmanager
+from threading import Lock
 from kubernetes import client, config, watch
 
 logging.basicConfig(
@@ -7,19 +12,44 @@ logging.basicConfig(
 logging.info("reading in-cluster config")
 config.load_incluster_config()
 logging.info("building client")
+logger_lock = Lock()
 v1 = client.CoreV1Api()
 
+class TimeoutException(Exception): pass
 
-def watch_ns():
-    w = watch.Watch()
-    try:
-        for event in w.stream(v1.list_namespace, timeout_seconds=2400):
-            logging.info("Event: %s %s" % (event['type'], event['object'].metadata.name))
-    except Exception as e:
-        logging.info("got exception:", e)
-        watch_ns()
-    logging.info("manually abort the watch")
+@contextmanager
+def time_limit(seconds):
+  def signal_handler(signum, frame):
+    raise TimeoutException("Timed out!")
+  signal.signal(signal.SIGALRM, signal_handler)
+  signal.alarm(seconds)
+  try:
+    yield
+  finally:
+    signal.alarm(0)
 
+def watchApi(id, type):
+  w = watch.Watch()
+  try:
+    with logger_lock:
+      logging.info(f"start {type} watch")
+    with time_limit(480):
+      for event in w.stream(id, timeout_seconds=360):
+        pass
+      #logging.info(f"{type} Event: %s %s" % (event['type'], event['object'].metadata.name))
+    with logger_lock:
+      logging.info(f"{type} watch shutdown")
+  except TimeoutException as e:
+    w.stop()
+    with logger_lock:
+      logging.info(f"!!! manually abort the {type} watch !!!!")
+  except Exception as e:
+    with logger_lock:
+      logging.info(f"{type} got exception:", e)    
+  watchApi(id, type)
+
+def main():
+  watchApi(v1.list_pod_for_all_namespaces, "pod")
 
 if __name__ == "__main__":
-    watch_ns()
+  main()
